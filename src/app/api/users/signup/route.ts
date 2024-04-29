7175f3ea3da4e386fc5760184c5494d5251d3b17
Diwakar Giri <diwakargiri23@gmail.com>
2024-04-30 00:24:44 +0530

    register route completed

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,52 @@
+import { connect } from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+import { sendEmail } from '@/helpers/mailer';
+
+connect();
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const reqBody = await req.json();
+    const { username, email, password } = reqBody;
+
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        { error: 'User Already Exist' },
+        { status: 400 }
+      );
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    //send verification email
+
+    await sendEmail({ email, emailType: 'VERIFY', userId: savedUser._id });
+
+    return NextResponse.json(
+      {
+        message: 'User Registered successfully',
+        success: true,
+        savedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,21 +1,39 @@
 import mongoose from 'mongoose';
 
-export const connect =  async () => {
-    try {
+export async function connect() {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URL}/nextauth`);
+    const connection = mongoose.connection;
 
-        mongoose.connect(process.env.MONGO_URL!);
-        const connection = mongoose.connection;
-
-        connection.on('Connected', ()=>{
-            console.log('MongoDb connected');
-        })
-        connection.on('error', (err)=>{
-            console.log('MongoDb connection error,Please make sure db is up and running',err);  
-            process.exit();          
-        })
-        
-    } catch (error) {
-        console.log('Something went wrong in connecting to DB : ', error);
-        
-    }
+    connection.on('connected', () => {
+      console.log('MongoDb connected');
+    });
+    connection.on('error', (err) => {
+      console.log(
+        'MongoDb connection error, Please make sure db is up and running' + err
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log('Something went wrong in connecting to DB : ', error);
+  }
 }
+
+// import mongoose from 'mongoose';
+// export const connect = async () => {
+//   try {
+//     const { connection } = await mongoose.connect(
+//       process.env.MONGODB_URL as string,
+//       {
+//         dbName: 'nextauth',
+//       }
+//     );
+
+//     console.log('db connected...');
+
+//     console.log('connected with host ', connection.host);
+//   } catch (error) {
+//     console.log('failed  to connect with database');
+//     console.log(error);
+//   }
+// };
