2a0b27567bf6fbc91753aec63fbfb5e3fc88bf53
Diwakar Giri <diwakargiri23@gmail.com>
2024-04-30 17:36:03 +0530

    middleware Done

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicRoute =
+    path === '/login' || path === '/signup' || path === '/verifyemail';
+  const token = request.cookies.get('token')?.value || '';
+  console.log(token);
+
+  if (isPublicRoute && token) {
+    return NextResponse.redirect(new URL('/profile', request.url));
+  }
+
+  if (!isPublicRoute && !token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/', '/signup', '/login', '/verifyemail', '/profile'],
+};
