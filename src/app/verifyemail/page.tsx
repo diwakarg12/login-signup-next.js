dac919bdd20e36e756436743de2b71002337707f
Diwakar Giri <diwakargiri23@gmail.com>
2024-05-02 13:45:17 +0530

    some bug fixes

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -6,15 +6,17 @@ import { toast } from 'react-hot-toast';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 
+type OnVerifyProps = {
+    onVerify: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+};
+
 const VerifyEmail = () => {
     const [loading, setLoading] = useState(false);
     const [verified, setVerified] = useState(false);
     const [error, setError] = useState(false);
     const [token, setToken] = useState('');
 
-    const searchParams = useSearchParams();
-
-    const onVerify = async (e: any) => {
+    const onVerify = async (e: React.MouseEvent<HTMLButtonElement>) => {
         try {
             e.preventDefault();
             setLoading(true);
@@ -44,37 +46,45 @@ const VerifyEmail = () => {
         setLoading(false);
     }, 2000)
 
+    return (
+        <div className="w-full h-[100vh] text-gray-300 font-mono flex flex-col items-center justify-center">
+            <div className='md:w-1/2 sm:w-11/12 xs:w-full py-12 flex flex-col items-center justify-center'>
+                {loading ? <Loader /> : null}
+                <h1 className='sm:text-4xl xs:text-2xl text-center font-semibold py-6'>Verify Your Email</h1>
+                <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quae amet ullam quibusdam architecto fugit nostrum aliquid odit placeat autem ipsa modi vero nihil eius nisi in rem quaerat, quas dolor. Alias placeat officiis quasi. Quasi sunt debitis quisquam repellat reprehenderit culpa perferendis numquam voluptates, quos libero maxime commodi neque ipsum quibusdam, a nostrum, dolorum nemo dolores officiis. Sequi obcaecati sapiente, neque aliquam atque quo consequatur, vel nulla delectus excepturi iure labore. Odio minima, non porro modi ex officiis! Nam porro deleniti mollitia, soluta esse quo perspiciatis cum odit veritatis odio rem nisi fugiat ab officia sint? Ad, omnis repellat.</p>
+                <Suspense fallback={<Loader />}>
+                    <VerifyEmailInner onVerify={onVerify} />
+                </Suspense>
+
+                {verified && (
+                    <div className='py-4 flex flex-col items-center'>
+                        <h1 className='text-5xl font-semibold'>Verified</h1>
+                        {token}
+                        <Link href='/login' className='text-blue-500 text-lg hover:underline'>go to Login</Link>
+                    </div>
+                )}
+                {error && (
+                    <h1 className='text-2xl font-semibold text-center py-4'>Something went wrong while Verifying user, please try again after some time</h1>
+                )}
+            </div>
+        </div>
+    )
+}
+
+const VerifyEmailInner = ({ onVerify }: { onVerify: (event: React.MouseEvent<HTMLButtonElement>) => Promise<void> }) => {
+    const searchParams = useSearchParams();
+    const [token, setToken] = useState('');
+
     useEffect(() => {
-        setError(false)
+        // setError(false)
         const urlToken = searchParams.get('token');
         setToken(urlToken || '');
     }, [searchParams])
 
     return (
-        <Suspense fallback={<Loader />}>
-            <div className="w-full h-[100vh] text-gray-300 font-mono flex flex-col items-center justify-center">
-                <div className='md:w-1/2 sm:w-11/12 xs:w-full py-12 flex flex-col items-center justify-center'>
-                    {loading ? <Loader /> : null}
-                    <h1 className='sm:text-4xl xs:text-2xl text-center font-semibold py-6'>Verify Your Email</h1>
-                    <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quae amet ullam quibusdam architecto fugit nostrum aliquid odit placeat autem ipsa modi vero nihil eius nisi in rem quaerat, quas dolor. Alias placeat officiis quasi. Quasi sunt debitis quisquam repellat reprehenderit culpa perferendis numquam voluptates, quos libero maxime commodi neque ipsum quibusdam, a nostrum, dolorum nemo dolores officiis. Sequi obcaecati sapiente, neque aliquam atque quo consequatur, vel nulla delectus excepturi iure labore. Odio minima, non porro modi ex officiis! Nam porro deleniti mollitia, soluta esse quo perspiciatis cum odit veritatis odio rem nisi fugiat ab officia sint? Ad, omnis repellat.</p>
-                    <button id="button" name="button" type="submit"  {...token === '' ? { disabled: true } : ''} className="flex justify-center items-center py-3 my-6 sm:w-96 xs:w-72 bg-gray-800 text-gray-100 outline-none outline-offset-0 border-none cursor-pointer transition-all duration-400 hover:bg-gray-100 hover:text-orange-500" onClick={onVerify}>
-                        <p className="font-medium text-xl transition-all duration-300">Verify</p>
-                    </button>
-
-                    {verified && (
-                        <div className='py-4 flex flex-col items-center'>
-                            <h1 className='text-5xl font-semibold'>Verified</h1>
-                            {token}
-                            <Link href='/login' className='text-blue-500 text-lg hover:underline'>go to Login</Link>
-                        </div>
-                    )}
-                    {error && (
-                        <h1 className='text-2xl font-semibold text-center py-4'>Something went wrong while Verifying user, please try again after some time</h1>
-                    )}
-
-                </div>
-            </div>
-        </Suspense>
+        <button id="button" name="button" type="submit"  {...token === '' ? { disabled: true } : ''} className="flex justify-center items-center py-3 my-6 sm:w-96 xs:w-72 bg-gray-800 text-gray-100 outline-none outline-offset-0 border-none cursor-pointer transition-all duration-400 hover:bg-gray-100 hover:text-orange-500" onClick={onVerify}>
+            <p className="font-medium text-xl transition-all duration-300">Verify</p>
+        </button>
     )
 }
 
